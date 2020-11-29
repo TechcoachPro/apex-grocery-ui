aff3db3a8a96aa4e98ba7ec01f9a913499914bfe
Joel Jeffery <odilichukwujoel@gmail.com>
2020-11-29 22:41:19 +0100

    dashboard design done

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, ElementRef } from '@angular/core';
+import { ScrollDetail } from '@ionic/core';
 
 @Component({
   selector: 'app-home',
@@ -6,7 +7,111 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  @ViewChild('header') header: ElementRef;
+  isScrolled = false;
+
+  slideOpts = {
+    slidesPerView: 2,
+    slidesPerColumn: 1,
+    slidesPerGroup: 1,
+    initialSlide: 0,
+    speed: 400
+  };
+
+  slideOptsCat = {
+    slidesPerView: 4,
+    slidesPerColumn: 1,
+    slidesPerGroup: 1,
+    initialSlide: 0,
+    speed: 400
+  };
+
+  slides = [
+    {
+      id: '1',
+      title: 'Rs. 500 Cashback',
+      desc: 'One treat order now',
+      img: '../../assets/offers/Cashback-PNG-Free-Image.png',
+      color: 'primary'
+    },
+    {
+      id: '1',
+      title: 'Minimum 30% Off!',
+      desc: 'On all products fruits and virgin',
+      img: '../../assets/offers/Healthy-Food-PNG-Picture.png',
+      color: 'secondary'
+    },
+    {
+      id: '1',
+      title: 'Fast Delivery',
+      desc: 'To all enlisted locations',
+      img: '../../assets/offers/Land-Rover-Range-Rover-PNG-Picture.png',
+      color: 'tertiary'
+    }
+  ];
+
+  categories = [
+    {
+      id: '1',
+      title: 'Burger',
+      img: '../../assets/categories/Tofu-Burger-Transparent.png',
+    },
+    {
+      id: '1',
+      title: 'Pancake',
+      img: '../../assets/categories/Pancake-PNG-HD-Image.png',
+    },
+    {
+      id: '1',
+      title: 'Noodles',
+      img: '../../assets/categories/Chinese-Noodles-PNG-Image.png',
+    },
+    {
+      id: '1',
+      title: 'Hamburger',
+      img: '../../assets/categories/Sandwich-Hamburger-PNG.png',
+    },
+    {
+      id: '1',
+      title: 'Fries',
+      img: '../../assets/categories/Fried-Chicken-PNG-Image.png',
+    },
+  ];
+
+  products = [
+    {
+      id: '1',
+      title: 'Stew Paste',
+      img: '../../assets/products/stew.jpg',
+    },
+    {
+      id: '1',
+      title: 'Fruit Castle',
+      img: '../../assets/products/food-fruit-castle-sweets.jpg',
+    },
+    {
+      id: '1',
+      title: 'French Burger',
+      img: '../../assets/products/resdefault.jpg',
+    },
+    {
+      id: '1',
+      title: 'Pasteries',
+      img: '../../assets/products/med-food-hero.jpg',
+    },
+    {
+      id: '1',
+      title: 'Harmburger',
+      img: '../../assets/products/fast-food-wallpaper-42089-43080-hd-wallpapers.jpg',
+    },
+  ];
 
   constructor() {}
 
+  async handleScroll(event: CustomEvent<ScrollDetail>) {
+    if (event && event.detail && event.detail.scrollTop) {
+      const scrollTop = event.detail.scrollTop;
+      this.isScrolled = scrollTop >= 50;
+    }
+  }
 }
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,17 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig: {
+    apiKey: 'AIzaSyCLLt1-_-zr_2Z2Qj7RiIrdT81TN5FKHk4',
+    authDomain: 'apexgrocery-9e156.firebaseapp.com',
+    databaseURL: 'https://apexgrocery-9e156.firebaseio.com',
+    projectId: 'apexgrocery-9e156',
+    storageBucket: 'apexgrocery-9e156.appspot.com',
+    messagingSenderId: '551974337427',
+    appId: '1:551974337427:web:23eae389b4ca44abe7c666',
+    measurementId: 'G-5FCE8WXL63'
+  }
 };
 
 /*
